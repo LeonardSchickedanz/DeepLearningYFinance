c58bc691a6a30ef01ecb144be5c9543376dedb51
Leonard Schickedanz <lsh@objektkultur.de>
2024-10-27 13:25:55 +0100

    received fundamental and timeseries data and converted it to raw excel, cleaned data and converted to clean excel successfully, also created the first 2 tensors

--- src/alphavantage_data.py
+++ src/alphavantage_data.py
@@ -38,37 +38,48 @@ pd.set_option('display.max_columns', None)
 pd.set_option('display.max_rows', None)
 pd.set_option('display.width', None)
 
-# create instances
-api_d_fundamental_data = FundamentalData(key=API_KEY) #load data once then comment out
-api_d_time_series = TimeSeries(key=API_KEY, output_format='pandas')
+def api_raw_data_to_excel():
+    # create instances
+    api_d_fundamental_data = FundamentalData(key=API_KEY)  # load data once then comment out
+    api_d_time_series = TimeSeries(key=API_KEY, output_format='pandas')
 
-d_quarterly_income, _ = api_d_fundamental_data.get_income_statement_quarterly(symbol='AAPL')
-d_time_series, _ = api_d_time_series.get_daily(symbol='AAPL', outputsize='compact')
+    d_quarterly_income, _ = api_d_fundamental_data.get_income_statement_quarterly(symbol='AAPL')
+    d_time_series, _ = api_d_time_series.get_daily(symbol='AAPL', outputsize='compact')  # 100 days
+
+    d_quarterly_income.to_excel('../data/d_fundamental_raw.xlsx', index=False)
+    d_time_series.to_excel('../data/d_timeseries_raw.xlsx', index=False)
+
+#api_raw_data_to_excel()
+
+
+d_quarterly_income = pd.read_excel('../data/d_fundamental_raw.xlsx')
+d_time_series = pd.read_excel('../data/d_timeseries_raw.xlsx')
+
+# clean data
+# d_time_series = d_time_series.drop(columns=['date'])
+d_quarterly_income = d_quarterly_income.drop(columns='fiscalDateEnding')
+d_quarterly_income = d_quarterly_income.drop(columns='depreciation')
+d_quarterly_income = d_quarterly_income.drop(columns='reportedCurrency')
+d_quarterly_income.replace("None", np.nan, inplace=True)
+d_quarterly_income = d_quarterly_income.fillna(0) # replaces ever None with 0
 
 d_quarterly_income.to_excel('../data/d_fundamental.xlsx', index=False)
-d_time_series.to_excel('../data/data_timeseries.xlsx', index=False)
+d_time_series.to_excel('../data/d_timeseries.xlsx', index=False)
 
 d_quarterly_income = pd.read_excel('../data/d_fundamental.xlsx')
-d_time_series = pd.read_excel('../data/data_timeseries.xlsx')
-
-d_time_series, _ = d_time_series.get_daily(symbol='AAPL', outputsize='compact') # 100 days
-
-d_quarterly_income, _ = d_quarterly_income.get_income_statement_quarterly(symbol='AAPL')
-d_latest_quarters = d_quarterly_income.head(8) # last 8 quarterly earnings
+d_time_series = pd.read_excel('../data/d_timeseries.xlsx')
 
 # create tensors
 f_time_series = 6 # open, high, low, close, adj close, volume
 f_quarterly_income = 20
 
-# remove date of data
-d_time_series = d_time_series.drop(columns=['date'])
-d_quarterly_income = d_quarterly_income.drop(columns='Fiscal Date Ending')
-
 # create tensors
 t_time_series = torch.tensor(d_time_series.values).float()
 t_quarterly_income = torch.tensor(d_quarterly_income.values).float()
 
-t_quarterly_income = t_quarterly_income.unsqueeze(0).expand(100, -1)  # transform to (100, 20) for cat
-t_combined = torch.cat((t_time_series, t_quarterly_income), dim=1) #(100, 26)
+#t_quarterly_income = t_quarterly_income.unsqueeze(0).expand(100, -1)  # transform to (100, 20) for cat
+#t_combined = torch.cat((t_time_series, t_quarterly_income), dim=1) #(100, 26)
 
 # now call model with 260 input neurons
+
+
