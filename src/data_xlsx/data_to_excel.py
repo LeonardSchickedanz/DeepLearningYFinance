37f9351f92f0317cbaae0c9786c797a76b2a3d39
leonschick <leon.schickedanz@gmail.com>
2024-11-09 16:45:02 +0100

    fixed stretch_data method

--- src/data_xlsx/data_to_excel.py
+++ src/data_xlsx/data_to_excel.py
@@ -27,6 +27,9 @@ d_time_series = pd.read_excel('../../data_xlsx/d_timeseries_raw.xlsx', index_col
 r_time_series = d_time_series
 d_time_series = d_time_series.reset_index()
 
+d_quarterly_income.to_excel('../../data_xlsx/d_quarterly_income1.xlsx', index=True)
+d_time_series.to_excel('../../data_xlsx/d_timeseries1.xlsx', index=True)
+
 
 # clean data_xlsx
 d_quarterly_income = d_quarterly_income.drop(columns='depreciation')
@@ -35,8 +38,11 @@ d_quarterly_income.replace("None", np.nan)
 d_quarterly_income = d_quarterly_income.fillna(0) # replaces ever None with 0
 d_quarterly_income['fiscalDateEnding'] = pd.to_datetime(d_quarterly_income['fiscalDateEnding'], errors='coerce')
 
-def stretch_data(data_frame, column_name, months):
-    new_df = pd.DataFrame(columns=data_frame.columns) # neue data_xlsx frame
+d_quarterly_income.to_excel('../../data_xlsx/d_quarterly_income2.xlsx', index=True)
+d_time_series.to_excel('../../data_xlsx/d_timeseries2.xlsx', index=True)
+
+def stretch_data(data_frame, column_name, months, today):
+    new_df = pd.DataFrame(columns=data_frame.columns)
     for i in range(months-1):
         row1 = data_frame.iloc[i]
         row2 = data_frame.iloc[i+1]
@@ -59,12 +65,21 @@ def stretch_data(data_frame, column_name, months):
         for i in range(0, len(upper_part)): # hänge alles vom oberen teil an new_df
             new_df.loc[len(new_df)] = upper_part.iloc[i]
 
+    while new_df.iloc[0, 0] < today:
+        head_row = new_df[0:1].copy()
+        head_row.iloc[0, 0] = head_row.iloc[0, 0] + pd.Timedelta(days=1)
+        new_df = pd.concat([head_row, new_df], ignore_index=True)
+
+    new_df = new_df.drop_duplicates(subset=['fiscalDateEnding']).sort_values(by='fiscalDateEnding').reset_index(drop=True)
     return new_df
 
 counter_months = d_quarterly_income.iloc[:, 0].dt.to_period('M').nunique()
-d_quarterly_income=stretch_data(d_quarterly_income, column_name = 'fiscalDateEnding', months=counter_months)
-d_quarterly_income = d_quarterly_income.drop_duplicates(subset=['fiscalDateEnding']).sort_values(by='fiscalDateEnding').reset_index(drop=True)
+d_quarterly_income=stretch_data(d_quarterly_income, column_name = 'fiscalDateEnding', months=counter_months, today=d_time_series.iloc[0][0])
+
 d_quarterly_income = d_quarterly_income.iloc[::-1].reset_index(drop=True)
+
+d_quarterly_income.to_excel('../../data_xlsx/d_quarterly_income3.xlsx', index=True)
+d_time_series.to_excel('../../data_xlsx/d_timeseries3.xlsx', index=True)
 
 # set unix time stamps
 d_quarterly_income['fiscalDateEnding'] = pd.to_datetime(d_quarterly_income['fiscalDateEnding'])
