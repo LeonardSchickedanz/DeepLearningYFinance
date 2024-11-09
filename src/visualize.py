847b894d219a71b3af7b15565ae2c579d83e6ab9
leonschick <leon.schickedanz@gmail.com>
2024-11-09 18:17:12 +0100

    added method plot_stocks

--- src/visualize.py
+++ src/visualize.py
@@ -1,9 +1,7 @@
-import pandas as pd
 import plotly.express as px
 import plotly.graph_objects as go
-import numpy as np
-#import data/data.py as d
-#import data as d
+import pandas as pd
+
 
 def plot_losses(losses, test_losses):
     fig = go.Figure()
@@ -40,11 +38,50 @@ def plot_losses(losses, test_losses):
     # Show plot
     fig.show()
 
+import plotly.graph_objects as go
+import pandas as pd
 
-#def plot_stocks(data):
+def plot_stocks(datum, wert1, wert2):
+    # Erstelle den Plot
+    fig = go.Figure()
 
-x = np.random.random(50)
-y = np.random.random(50)
+    # Hinzufügen der ersten Datenreihe als Linie
+    fig.add_trace(go.Scatter(
+        x=datum,
+        y=wert1,
+        mode='lines',
+        name='Stock Price 1',
+        line=dict(color='blue')  # Farbe für die erste Reihe
+    ))
 
-fig = px.scatter(x,y)
-fig.show()
+    # Hinzufügen der zweiten Datenreihe als Linie
+    fig.add_trace(go.Scatter(
+        x=datum,
+        y=wert2,
+        mode='lines',
+        name='Stock Price 2',
+        line=dict(color='red')  # Farbe für die zweite Reihe
+    ))
+
+    # Layout anpassen
+    fig.update_layout(
+        title='Stock Prices Over Time',
+        xaxis_title='Date',
+        yaxis_title='Price',
+        template='plotly_white'
+    )
+
+    # Achsenoptionen und Gitterlinien anpassen
+    fig.update_xaxes(showgrid=True, gridwidth=1, gridcolor='LightGray')
+    fig.update_yaxes(showgrid=True, gridwidth=1, gridcolor='LightGray')
+
+    # Plot anzeigen
+    fig.show()
+
+# Methode aufrufen
+d_time_series = pd.read_excel('../data_xlsx/d_timeseries_raw.xlsx', index_col=0)
+datum=d_time_series.index
+wert = d_time_series['4. close']
+wert2 = wert + 100
+plot_stocks(datum, wert, wert2)
+
