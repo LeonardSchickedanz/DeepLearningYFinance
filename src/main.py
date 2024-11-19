4395ee4d94b0917cc9227c3e221fa97b7ad07266
leonschick <leon.schickedanz@gmail.com>
2024-11-19 21:07:53 +0100

    added csv files, added run method

--- src/main.py
+++ src/main.py
@@ -13,55 +13,55 @@ model = model_class.LSTMModel(inputL=f_input, hiddenL1=150, hiddenL2=150, hidden
 criterion = torch.nn.MSELoss()
 optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
 
-# get the data and scaler
-x_train, x_test, y_train, y_test, rest_scaler, price_scaler = data.prepare_training_data(data.t_combined)
+def run():
+    # get the data and scaler
+    x_train, x_test, y_train, y_test, rest_scaler, price_scaler = data.prepare_training_data(data.t_combined)
 
-print("x_train:", x_train.shape)
-print("x_test:", x_test.shape)
-print("y_train:", y_train.shape)
-print("y_test:", y_test.shape)
-print("\n")
+    print("x_train:", x_train.shape)
+    print("x_test:", x_test.shape)
+    print("y_train:", y_train.shape)
+    print("y_test:", y_test.shape)
+    print("\n")
 
-# Training loop
-model.train()
-#epochs = x_train.size(1)
-epochs = 100
-losses = []
-test_losses = []
-prediction = []
-
-for i in range(epochs):
-    # training
-    y_pred = model(x_train[:,:,:])
-
-    loss = criterion(y_pred, y_train)
-    losses.append(loss.item())
-    print(i)
-    # validation
-    model.eval()
-    with torch.no_grad():
-        y_pred_test = model(x_test)
-        test_loss = criterion(y_pred_test, y_test)
-        test_losses.append(test_loss.item())
+    # Training loop
     model.train()
+    #epochs = x_train.size(1)
+    epochs = 1
+    losses = []
+    test_losses = []
+    prediction = []
 
-    optimizer.zero_grad()
-    loss.backward()
-    optimizer.step()
-    prediction = y_pred_test
+    for i in range(epochs):
+        # training
+        y_pred = model(x_train[:,:,:])
+
+        loss = criterion(y_pred, y_train)
+        losses.append(loss.item())
+        print(i)
+        # validation
+        model.eval()
+        with torch.no_grad():
+            y_pred_test = model(x_test)
+            test_loss = criterion(y_pred_test, y_test)
+            test_losses.append(test_loss.item())
+        model.train()
+
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+        prediction = y_pred_test
+
+    return prediction, losses, test_losses, y_test, price_scaler
+
+prediction, losses, test_losses, y_test, price_scaler = run()
 
 # save data
-np_y_pred = y_pred.numpy()
+np_y_pred = prediction.detach().numpy()
 df = pd.DataFrame(np_y_pred)
-df.to_csv("../model_output/y_pred.csv", index=False)
+df.to_csv("../model_output/prediction.csv", index=False)
 
-np_losses = losses.numpy()
-df = pd.DataFrame(np_losses)
-df.to_csv("../model_output/losses.csv", index=False)
-
-np_test_losses = test_losses.numpy()
-df = pd.DataFrame(np_test_losses)
-df.to_csv("../model_output/test_losses.csv", index=False)
+pd.DataFrame(losses).to_csv("../model_output/losses.csv", index=False)
+pd.DataFrame(test_losses).to_csv("../model_output/test_losses.csv", index=False)
 
 # plot
 v.plot_losses(losses, test_losses)
