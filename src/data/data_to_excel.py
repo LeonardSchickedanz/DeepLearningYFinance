1e8c57591cae784ec890ed5ba80782935ed436cb
leonschick <leon.schickedanz@gmail.com>
2024-12-10 18:16:00 +0100

    data_to_excel.py remade, all data points now create raw and processed excel

--- src/data/data_to_excel.py
+++ src/data/data_to_excel.py
@@ -1,6 +1,3 @@
-import json
-from token import EQUAL
-
 import numpy as np
 from alpha_vantage.timeseries import TimeSeries
 from alpha_vantage.fundamentaldata import FundamentalData
@@ -31,14 +28,25 @@ def api_raw_data_to_excel():
     d_quarterly_income.to_excel(f'{directory_raw}d_quarterly_income_raw.xlsx', index=True)
     d_time_series.to_excel(f'{directory_raw}d_timeseries_raw.xlsx', index=True)
 
+economic_indicators = (
+    'd_real_gdp',
+    'd_real_gdp_per_capita',
+    'd_federal_funds_rate',
+    'd_cpi',
+    'd_inflation',
+    'd_retail_sales',
+    'd_durables',
+    'd_unemployment',
+    'd_nonfarm_payroll'
+    )
+
 def economic_indicators_to_excel():
     main_url = 'https://www.alphavantage.co/query?function='
     api_key_url = f'&apikey={API_KEY}'
 
-    url_list = [
+    url_list = (
         'REAL_GDP&interval=annual',
         'REAL_GDP_PER_CAPITA',
-        'TREASURY_YIELD&interval=daily&maturity=10year',
         'FEDERAL_FUNDS_RATE&interval=monthly',
         'CPI&interval=monthly',
         'INFLATION',
@@ -46,27 +54,18 @@ def economic_indicators_to_excel():
         'DURABLES',
         'UNEMPLOYMENT',
         'NONFARM_PAYROLL'
-    ]
-
-    economic_indicators = [
-        'd_real_gdp',
-        'd_real_gdp_per_capita',
-        'd_treasury_yield',
-        'd_federal_funds_rate',
-        'd_cpi',
-        'd_inflation',
-        'd_retail_sales',
-        'd_durables',
-        'd_unemployment',
-        'd_nonfarm_payroll'
-    ]
+    )
 
     dataframes = []
 
     for idx1, u in enumerate(url_list):
-        url =  f'{main_url}{u}{api_key_url}'
-        r = requests.get(url)
-        data = r.json()
+        try:
+            url =  f'{main_url}{u}{api_key_url}'
+            r = requests.get(url)
+            data = r.json()
+        except requests.exceptions.RequestException as e:
+            print(f"Error fetching data for {u}: {e}")
+            continue
 
         dates = []
         values = []
@@ -91,23 +90,22 @@ def economic_indicators_to_excel():
 #api_raw_data_to_excel()
 
 DATA_LIST = [
-    pd.read_excel(f'{directory_raw}d_cpi_raw.xlsx', index_col=0),
-    pd.read_excel(f'{directory_raw}d_durables_raw.xlsx', index_col=0),
+    pd.read_excel(f'{directory_raw}d_real_gdp_raw.xlsx', index_col=0),
+    pd.read_excel(f'{directory_raw}d_real_gdp_per_capita_raw.xlsx', index_col=0),
     pd.read_excel(f'{directory_raw}d_federal_funds_rate_raw.xlsx', index_col=0),
+    pd.read_excel(f'{directory_raw}d_cpi_raw.xlsx', index_col=0),
     pd.read_excel(f'{directory_raw}d_inflation_raw.xlsx', index_col=0),
+    pd.read_excel(f'{directory_raw}d_retail_sales_raw.xlsx', index_col=0),
+    pd.read_excel(f'{directory_raw}d_durables_raw.xlsx', index_col=0),
+    pd.read_excel(f'{directory_raw}d_unemployment_raw.xlsx', index_col=0),
     pd.read_excel(f'{directory_raw}d_nonfarm_payroll_raw.xlsx', index_col=0),
     pd.read_excel(f'{directory_raw}d_quarterly_income_raw.xlsx', index_col=0),
-    pd.read_excel(f'{directory_raw}d_real_gdp_per_capita_raw.xlsx', index_col=0),
-    pd.read_excel(f'{directory_raw}d_real_gdp_raw.xlsx', index_col=0),
-    pd.read_excel(f'{directory_raw}d_retail_sales_raw.xlsx', index_col=0),
     pd.read_excel(f'{directory_raw}d_timeseries_raw.xlsx', index_col=0),
-    pd.read_excel(f'{directory_raw}d_treasury_yield_raw.xlsx', index_col=0),
-    pd.read_excel(f'{directory_raw}d_unemployment_raw.xlsx', index_col=0)
-]
+    ]
 
 # CLEAN DATA
-d_quarterly_income = DATA_LIST[5]
-d_time_series = DATA_LIST[9]
+d_quarterly_income = DATA_LIST[9]
+d_time_series = DATA_LIST[10]
 
 d_time_series = d_time_series.reset_index()
 d_quarterly_income = d_quarterly_income.drop(columns='depreciation')
@@ -115,51 +113,62 @@ d_quarterly_income = d_quarterly_income.drop(columns='reportedCurrency')
 d_quarterly_income.replace("None", np.nan)
 d_quarterly_income = d_quarterly_income.fillna(0) # replaces every None with 0
 d_quarterly_income.rename(columns={'fiscalDateEnding': 'date'}, inplace=True)
-d_quarterly_income['date'] = pd.to_datetime(d_quarterly_income['fiscalDateEnding'], errors='coerce')
+
+DATA_LIST[9] = d_quarterly_income
+DATA_LIST[10] = d_time_series
+
 
 # STRETCH DATA
-def stretch_data(data, min_date, max_date, date_column):
-    data[date_column] = pd.to_datetime(data[date_column])
+def stretch_data(data, min_d, max_d, date_column = 'date'):
     data.set_index(date_column, inplace=True)
     data.sort_index(inplace=True)
     data = data.resample('D').ffill()
-    date_range = pd.date_range(start=min_date, end=max_date, freq='D')
+    date_range = pd.date_range(start=min_d, end=max_d, freq='D')
     data = data.reindex(date_range)
     data = data.reset_index(names=['date'])
     data = data.iloc[::-1].reset_index(drop=True)
 
     return data
 
-min_date = max(DATA_LIST[0]['date'],DATA_LIST[1]['date'],
-               DATA_LIST[2]['date'],DATA_LIST[3]['date'],
-               DATA_LIST[4]['date'],DATA_LIST[5]['date'],
-               DATA_LIST[6]['date'],DATA_LIST[7]['date'],
-               DATA_LIST[8]['date'],DATA_LIST[9]['date'],
-               DATA_LIST[10]['date'],DATA_LIST[11]['date'])
-
-max_date = min(DATA_LIST[0]['date'],DATA_LIST[1]['date'],
-               DATA_LIST[2]['date'],DATA_LIST[3]['date'],
-               DATA_LIST[4]['date'],DATA_LIST[5]['date'],
-               DATA_LIST[6]['date'],DATA_LIST[7]['date'],
-               DATA_LIST[8]['date'],DATA_LIST[9]['date'],
-               DATA_LIST[10]['date'],DATA_LIST[11]['date'])
-
-for idx, df in enumerate(DATA_LIST):
-    DATA_LIST[idx] = stretch_data(data=df,min_date=min_date ,max_date=max_date , date_column=0)
+for df in DATA_LIST:
+    df['date'] = pd.to_datetime(df['date'])
+date_columns = [df['date'] for df in DATA_LIST]
+max_d = min(df['date'].max() for df in DATA_LIST)
+min_d = max(df['date'].min() for df in DATA_LIST)
 
 # UNIX TIME STAMPS
-d_quarterly_income['date'] = pd.to_datetime(d_quarterly_income['date'])
-d_quarterly_income['date'] = (d_quarterly_income['date'] - pd.Timestamp('1970-01-01')).dt.total_seconds()
-d_time_series['date'] = pd.to_datetime(d_time_series['date'])
-d_time_series['date'] = (d_time_series['date'] - pd.Timestamp('1970-01-01')).dt.total_seconds()
+def date_to_unix_time_stamp(data, date_column='date'):
+    data[date_column] = pd.to_datetime(data[date_column])
+    data[date_column] = (data[date_column] - pd.Timestamp('1970-01-01')).dt.total_seconds()
+    return data
 
-assert len(d_time_series) == len(d_quarterly_income)
+for idx1, df1 in enumerate(DATA_LIST):
+    DATA_LIST[idx1] = stretch_data(data=df1,min_d=min_d ,max_d=max_d)
+    DATA_LIST[idx1] = date_to_unix_time_stamp(DATA_LIST[idx1])
 
-# check data
-for idx in range(len(d_time_series)):
-    time_series_date = d_time_series.iloc[idx]['date']
-    quarterly_income_date = d_quarterly_income.iloc[idx]['date']
-    assert time_series_date == quarterly_income_date, f"DIFFERENCE FOUND d_time_series['date'] = {time_series_date}, d_quarterly_income['date'] = {quarterly_income_date} (Index: {idx})"
+# CHECK DATA
+assertion_length = len(DATA_LIST[0]['date'])
+for idx2, df2 in enumerate(DATA_LIST):
+    if len(df2) != assertion_length:
+        raise Exception(f'Data does not have same length: {idx2}')
 
-d_quarterly_income.to_excel(f'{directory_processed}d_quarterly_income.xlsx', index=True)
-d_time_series.to_excel(f'{directory_processed}d_timeseries.xlsx', index=True)
+for idx3 in range(assertion_length):
+   assert (
+       assertion_length == DATA_LIST[1]['date'][idx3] and
+       assertion_length == DATA_LIST[2]['date'][idx3] and
+       assertion_length == DATA_LIST[3]['date'][idx3] and
+       assertion_length == DATA_LIST[4]['date'][idx3] and
+       assertion_length == DATA_LIST[5]['date'][idx3] and
+       assertion_length == DATA_LIST[6]['date'][idx3] and
+       assertion_length == DATA_LIST[7]['date'][idx3] and
+       assertion_length == DATA_LIST[8]['date'][idx3] and
+       assertion_length == DATA_LIST[9]['date'][idx3] and
+       assertion_length == DATA_LIST[10]['date'][idx3]
+   ), f'DIFFERENCE FOUND: {idx3}'
+
+# DATA TO EXCEL
+for idx in range(len(DATA_LIST)-2):
+    DATA_LIST[idx].to_excel(f'{directory_processed}{economic_indicators[idx]}.xlsx')
+DATA_LIST[9].to_excel(f'{directory_processed}d_quarterly_income.xlsx')
+DATA_LIST[10].to_excel(f'{directory_processed}d_timeseries.xlsx')
+
