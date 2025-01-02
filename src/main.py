fixed scaling bug

src/main.py
@@ -1,5 +1,7 @@
 from datetime import datetime
 import numpy as np
+from sklearn.utils.multiclass import type_of_target
+
 from data import data
 import model as model_class
 import torch
@@ -11,9 +13,8 @@ model = model_class.LSTMModel(inputL = data.T_COMBINED.shape[1], hiddenL1=100, h
 criterion = torch.nn.MSELoss()
 optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
 
-
 def run():
-    x_train, x_test, y_train, y_test, rest_scaler, price_scaler = data.prepare_training_data(data.T_COMBINED)
+    x_train, x_test, y_train, y_test, main_scaler, price_scaler = data.prepare_training_data(data.T_COMBINED)
 
     print("x_train:", x_train.shape)
     print("x_test:", x_test.shape)
@@ -23,10 +24,11 @@ def run():
     train_dataset = torch.utils.data.TensorDataset(x_train, y_train)
     train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=32, shuffle=True)
 
-    epochs = 100
+    epochs = 3
     losses = []
     test_losses = []
     prediction = []
+    final_prediction = []
 
     for epoch in range(epochs):
         model.train()
@@ -56,19 +58,26 @@ def run():
             hidden_state_val = torch.zeros(1, batch_size_test, model.lstm.hidden_size).to(x_test.device)
             cell_state_val = torch.zeros(1, batch_size_test, model.lstm.hidden_size).to(x_test.device)
 
-            y_pred_test, _ = model(x_test, hidden_state_val, cell_state_val)
+            y_pred_test, _ = model.forward(x_test, hidden_state_val, cell_state_val)
             test_loss = criterion(y_pred_test, y_test)
             test_losses.append(test_loss.item())
 
-            prediction.append(y_pred_test.detach().numpy())
+            prediction.append(y_pred_test.detach().numpy().flatten())
+            #print("y_red_test: ",y_pred_test.detach().numpy().flatten())
+
+            if epoch == epochs - 1:
+                final_prediction = y_pred_test.detach().numpy().flatten()
+                #print(final_prediction)
 
         print(f"Epoch {epoch}: Train Loss = {avg_loss}, Test Loss = {test_loss.item()}")
+        print(f'final_prediction length: {len(final_prediction)}')
 
-    prediction = np.vstack(prediction) if len(prediction) > 0 else None
+    return final_prediction, losses, test_losses, y_test, main_scaler, price_scaler
 
-    return prediction, losses, test_losses, y_test, price_scaler
+prediction, losses, test_losses, y_test, main_scaler, price_scaler = run()
 
-prediction, losses, test_losses, y_test, price_scaler = run()
+#print(type(prediction))
+#print("prediction[0]", prediction[0])
 
 # save data
 np_y_pred = np.array(prediction)
@@ -89,23 +98,17 @@ date = date[-len(y_test):]
 v.plot_stocks(date, y_test, prediction, scaler=price_scaler)
 
 def evaluate_prediction(actual, forecast):
-    # Berechne die Differenz zwischen tatsächlichen Werten und Vorhersagen
     diff = np.array(actual) - np.array(forecast)
-
-    # Berechne den mittleren absoluten Fehler (MAE)
     mae = np.mean(np.abs(diff))
-
-    # Berechne den mittleren quadratischen Fehler (MSE)
     mse = np.mean(diff ** 2)
-
-    # Berechne die Wurzel des mittleren quadratischen Fehlers (RMSE)
     rmse = np.sqrt(mse)
+    mape = np.mean(np.abs((actual - forecast) / actual)) * 100
 
-    # Berechne den Determinationskoeffizienten (R-Quadrat)
     r_squared = 1 - (np.sum((actual - forecast) ** 2) / np.sum((actual - np.mean(actual)) ** 2))
-    print(f'MAE: {mae}')
-    print(f'MSE: {mse}')
-    print(f'RMSE: {rmse}')
+    print(f'mean absolut error: {mae}')
+    print(f'mean squared error: {mse}')
+    print(f'root mean squared error: {rmse}')
     print(f'R-Squared: {r_squared}')
+    print(f'mean absolute percentage error: {mape}')
 
-evaluate_prediction(y_test, prediction)
+#evaluate_prediction(y_test, prediction)

src/visualize.py
@@ -34,21 +34,30 @@ def plot_losses(losses, test_losses):
     fig.show()
 
 import plotly.graph_objects as go
+import numpy as np
+
 
 def plot_stocks(dates, y_test, y_pred_test, scaler):
+    if len(y_test) != len(y_pred_test):
+        print(f"y_test ({len(y_test)}) & y_pred_test ({len(y_pred_test)}) do not have the same length")
+    if scaler is None:
+        print("scaler is none")
 
-    if len(y_test) != len(y_pred_test): print(f"y_test ({len(y_test)}) & y_pred_test ({len(y_pred_test)}) do not have the same length")
-    if scaler is None: print("scaler is none")
+    y_test = np.array(y_test)
+    y_pred_test = np.array(y_pred_test)
 
-    y_test_transformed = scaler.inverse_transform(y_test.numpy().reshape(-1, 1))
+    # descaling
+    y_test_transformed = scaler.inverse_transform(y_test.reshape(-1, 1))
     y_pred_transformed = scaler.inverse_transform(y_pred_test.reshape(-1, 1))
 
+    # inverse
+    y_test_transformed = y_test_transformed[::-1]
+    y_pred_transformed = y_pred_transformed[::-1]
 
-    # Stelle sicher, dass alle Arrays die gleiche Länge haben
-    #min_len = min(len(dates), len(y_test_transformed), len(y_pred_transformed))
-    #dates = dates[-min_len:]  # Nimm die letzten min_len Einträge
-    #y_test_transformed = y_test_transformed[-min_len:]
-    #y_pred_transformed = y_pred_transformed[-min_len:]
+    min_len = min(len(dates), len(y_test_transformed), len(y_pred_transformed))
+    dates = dates[-min_len:]
+    y_test_transformed = y_test_transformed[-min_len:]
+    y_pred_transformed = y_pred_transformed[-min_len:]
 
     fig = go.Figure()
 